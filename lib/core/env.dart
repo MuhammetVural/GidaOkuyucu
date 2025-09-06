b2eb66b3c7aadbd6293729fbcf230956f05b732c
Muhammet Vural <muhammetvural@anadolu.edu.tr>
2025-09-06 20:29:32 +0300

    stop tracking env.dart

--- lib/core/env.dart
+++
@@ -1,9 +0,0 @@
-// lib/core/env.dart
-class Env {
-  // TODO: Supabase projenin değerlerini buraya yaz
-  static const supabaseUrl = 'https://ajydtfsnaturoabtetjr.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqeWR0ZnNuYXR1cm9hYnRldGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNTI2MDQsImV4cCI6MjA3MjcyODYwNH0.TJl-Pm2wlxuSSp-RXk5LQEWOuRvR3TzSGZ2nfXyuSxQ';
-
-  // DB tablo isimleri
-  static const tableProducts = 'products';
-}
